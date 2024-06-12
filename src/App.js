ac660bd38e426648210530fa9e47ec5adfe2f1eb
AnnaNThomas <annathomas2003ned@gmail.com>
2024-06-13 00:34:15 +0530

    Viewvehicle cmplt

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Addvehicle from './components/Addvehicle';
 import Searchvehicle from './components/Searchvehicle';
+import Viewvehicle from './components/Viewvehicle';
 
 function App() {
   return (
     <div>
       <Addvehicle/>
       <Searchvehicle/>
+      <Viewvehicle/>
     </div>
   );
 }
